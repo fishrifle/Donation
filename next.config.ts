4e9941b94e3a300912591a4b2dd052b2e6cf002d
jdavis-banyanlabs <jdavis@banyanlabs.io>
2025-07-28 13:43:45 -0500

    feat:Made changes for i-frame

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+   return [
+    {
+        source: '/(.*)',
+         headers: [
+          {
+             key: 'X-Frame-Options',
+             value: 'ALLOWALL',
+             },
+             ],
+            },
+          ];
+        },
+      };
+      export default nextConfig;
 
-export default nextConfig;
